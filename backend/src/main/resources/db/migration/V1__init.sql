38ba35df5e696118d2523a383e3cfc58aa9568b6
JinHoooooou <jinho4744@naver.com>
2026-04-15 17:00:47 +0900

    db: waiting_entries visitor_name 컬럼 phone_number로 변경

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -31,7 +31,7 @@ CREATE TABLE waiting_entries
 (
   id             UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
   store_id       UUID        NOT NULL REFERENCES stores (id),
-  visitor_name   VARCHAR(50) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
   party_size     INT         NOT NULL CHECK (party_size BETWEEN 1 AND 10),
   waiting_number INT         NOT NULL,
   status         VARCHAR(20) NOT NULL DEFAULT 'WAITING',
